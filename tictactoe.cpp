092066154156c22326e36f3528a14a014c31a584
Khoa Phan <khoaphan0897@gmail.com>
2019-05-23 11:39:49 +0700

    Added dynamic board, AI still bugs

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-int board[3][3];
-int counter = 0;
+
+int** board = new int*[3];
+
 using namespace std;
 //Score
 //human = 1
 //blank = 0
 //computer = -1
-void drawBoard()
+void drawBoard(int** board)
 {
 	for(int i = 0; i < 3; i++)
 	{
@@ -55,13 +56,13 @@ int checkWin()
 	return 0;
 }
 
-void duplicateBoard(int board[3][3], int board2[3][3])
+void duplicateBoard(int** board, int** board2)
 {
 	for(int i = 0; i < 3; i++)
 		for(int j = 0; j < 3; j++) board2[i][j] = board[i][j];
 }
 
-int minimax(int board[3][3],int player)//algorithm used to check
+int minimax(int** board,int player)//algorithm used to check
 {
 	//Player 1 = human = 1
 	//PLayer 2 = bot = -1
@@ -70,18 +71,22 @@ int minimax(int board[3][3],int player)//algorithm used to check
 
 	int move = -1;
 	int score = -2;
-	for(int i = 0; i < 9; i++)
+	for(int i = 0; i < 3; i++)
 	{
-		for(int j = 0; j < 9; j++)
+		for(int j = 0; j < 3; j++)
 		{
 			if(board[i][j] == 0)
 			{
-				int board2[3][3];
+				int** board2 = new int*[3];
 				cout << board2 << endl;
-				duplicateBoard(board,board2);
+				for(int i = 0; i < 3; ++i)
+   			 		board2[i] = new int[3];
+   			 	duplicateBoard(board, board2);
+   			 	drawBoard(board2);
 				if(player == 1) board2[i][j] = 1;
 				if(player == -1) board2[i][j] = -1;
 				int scoreForMove = minimax(board2,(player*-1));
+				delete[] board2;
 				if(scoreForMove > score) {
 					score = scoreForMove;
 					move = i;
@@ -89,6 +94,7 @@ int minimax(int board[3][3],int player)//algorithm used to check
 			}
 		}
 	}
+	return move;
 
 	if (move == -1) return 0;
 	return score;
@@ -97,22 +103,30 @@ int minimax(int board[3][3],int player)//algorithm used to check
 
 int main()
 {
+	for(int i = 0; i < 3; ++i)
+    board[i] = new int[3];
 
+	for(int i = 0; i < 3; i++)
+		for(int j = 0; j < 3; j++) board[i][j] =0;
 	while(1)
 	{
 		int i;
 		int j;
-		drawBoard();
+		drawBoard(board);
 		cout << "It's your turn" << endl;
 		cin >> i;
 		cin >> j;
 		board[i][j] = 1;
-		drawBoard();
+		drawBoard(board);
 		if(checkWin())
 			break;
 		cout << "It's bots turn" << endl;
-		cout << minimax(board,-1) << endl;
-		drawBoard();
+		int x = minimax(board,-1);
+		cout << x << endl;
+		i = x/3;
+		j = x%3;
+		board[i][j] = -1;
+		drawBoard(board);
 		if(checkWin())
 			break;
 	}
