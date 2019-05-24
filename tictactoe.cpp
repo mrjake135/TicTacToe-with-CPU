0ccfc9dedba24d04fb9e3ebb8927c6b5ae937684
Khoa Phan <khoaphan0897@gmail.com>
2019-05-24 13:21:28 +0700

    everything looks good, need to add finishing touches the commit message for your changes. Lines starting

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -20,7 +20,6 @@ void drawBoard(int* board)
 	cout<< endl;
 }
 
-
 int checkWin(int* board)
 {
 	int total = 0;
@@ -51,9 +50,10 @@ int checkWin(int* board)
 	if(total == 3 | total == -3) return total/3;
 	int move = -1;
 	for(int i = 0; i < 9; ++i) 
-		if(board[i] != 0) move = 1;
+		if(board[i] == 0) move = 1;
 	if(move == -1) return 0;
-	else return -2;
+	
+	return -2;
 }
 
 void duplicateBoard(int* board, int* board2)
@@ -65,35 +65,75 @@ int minimax(int* board,int player)//algorithm used to check
 {
 	//Player 1 = human = -1
 	//PLayer 2 = bot = 1
-	if(checkWin(board) == 0) return 0;
-	if(checkWin(board) == 1 || checkWin(board) == -1) return 1;
-	int move = -1; int score = -2; int scoreForMove;
-	for(int i = 0; i < 9; ++i)
+	if(checkWin(board) == 0) 
+		return 0;
+	if(checkWin(board) == 1)
+		return 1;
+	if(checkWin(board) == -1) 
+		return -1;
+	
+	int scoreForMove;
+	if(player == 1)
 	{
-		if(board[i] == 0)
+		scoreForMove = INT_MIN;
+		for(int i = 0; i < 9; ++i)
 		{
 			int* board2 = new int[9]; //create new board to test on it
-		 	duplicateBoard(board, board2);
-		 	drawBoard(board2);
-			if(player == 1) board2[i] = 1;
-			if(player == -1) board2[i] = -1;
-			scoreForMove = -(minimax(board2,(player*-1)));
-			if(scoreForMove > score) 
+			duplicateBoard(board, board2);
+			if(board2[i] == 0)
 			{
-				score = scoreForMove;
-				move = i;
+				board2[i] = 1;
+				scoreForMove = max(scoreForMove,minimax(board2,(player*-1)));
 			}
 			delete[] board2;
 		}
 	}
-	return move;
+	if(player == -1)
+	{
+		scoreForMove = INT_MAX;
+		for(int i = 0; i < 9; ++i)
+		{
+			int* board2 = new int[9]; //create new board to test on it
+			duplicateBoard(board, board2);
+			if(board2[i] == 0)
+			{
+				board2[i] = -1;
+				scoreForMove = min(scoreForMove,minimax(board2,(player*-1)));
+			}
+			delete[] board2; //delete board
+		}
+	}
 
+	return scoreForMove;
+}
+
+int chooseBestMove(int* board)
+{
+	int bestMoveScore = INT_MIN;
+	int bestMove = -1;
+	for(int i = 0; i < 9;i++)
+	{
+		int* board2 = new int[9]; //create new board to test on it
+		duplicateBoard(board, board2);
+		if(board2[i] == 0) 
+		{
+			board2[i] = 1;
+			int newScore = minimax(board2,-1);
+			if(newScore > bestMoveScore)
+			{
+				bestMoveScore = newScore;
+				bestMove = i;
+				cout << bestMove << endl;
+			}
+		}
+		delete[] board2;
+	}
+	cout << "this is the best move " << bestMove << endl;
+	return bestMove;
 }
 
 int main()
 {
-	
-
 	for(int i = 0; i < 9; i++) board[i]=0;
 	while(1)
 	{
@@ -105,10 +145,10 @@ int main()
 		board[i] = -1;
 		if(checkWin(board) == 1 || checkWin(board) == -1 || checkWin(board) == 0)
 			break;
-		drawBoard(board);
 		cout << "It's bots turn" << endl;
-		int x = minimax(board,1);
+		int x = chooseBestMove(board);
 		board[x] = 1;
+		drawBoard(board);
 		if(checkWin(board) == 1 || checkWin(board) == -1 || checkWin(board) == 0)
 			break;
 	}
@@ -126,5 +166,3 @@ int main()
 	}
 	return 0;
 }
-
-
